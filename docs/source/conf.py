48ebbf8a03aaac0974ddb86b73ddaebc230edded
barronh <barronh@gmail.com>
2024-04-15 11:39:05 -0400

    RTD folding is not working right.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ extensions = [
     'matplotlib.sphinxext.plot_directive',
     'sphinx_copybutton',
     'sphinx_design',
-    'sphinx_rtd_theme',
+    #'sphinx_rtd_theme',
     'myst_nb',
     'sphinx_gallery.gen_gallery',
     'sphinx.ext.napoleon',
@@ -96,7 +96,7 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
