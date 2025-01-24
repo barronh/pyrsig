ba2d9ddd4d9628667a186f726127fe8f78dfb162
barronh <barronh@gmail.com>
2025-01-24 13:46:38 -0500

    update cmaq-tropomi

--- examples/comparisons/pair_cmaqtropomi.py
+++ examples/comparisons/pair_cmaqtropomi.py
@@ -19,7 +19,9 @@ integral at each hour.
 # Import libraries
 # ----------------
 # And tell xarray to keep variable attributes
+import matplotlib.pyplot as plt
 import pyrsig
+import pycno
 import pandas as pd
 import xarray as xr
 
@@ -31,11 +33,11 @@ xr.set_options(keep_attrs=True)
 # - area/time/domain of interest
 
 bbox = (-130, 20, -55, 60)
-start = '2019-07-01T00Z'
-end = '2019-07-01T00Z'
-gdnam = '36US3'
-spc = 'HCHO'  # or NO2
-qtmpl = 'CCTM_PHOTDIAG1_%Y%m%d.nc'
+start = '2019-06-01T00Z'
+end = '2019-06-07T00Z'
+gdnam = '12US1'
+spc = 'NO2'  # or NO2
+# qtmpl = 'CCTM_PHOTDIAG1_%Y%m%d.nc'
 
 # %%
 # Set species options
@@ -44,7 +46,11 @@ qtmpl = 'CCTM_PHOTDIAG1_%Y%m%d.nc'
 
 tdkey = {
     'HCHO': 'tropomi.offl.hcho.formaldehyde_tropospheric_vertical_column',
-    'NO2': 'tropomi.offl.hcho.formaldehyde_tropospheric_vertical_column'
+    'NO2': 'tropomi.offl.no2.nitrogendioxide_tropospheric_column'
+}[spc]
+cdkey = {
+    'HCHO': 'cmaq.equates.conus.integrated.HCHO_COLUMN',
+    'NO2': 'cmaq.equates.conus.integrated.NO2_COLUMN',
 }[spc]
 tvkey = spc
 qvkey = f'{spc}_COLUMN'
@@ -53,14 +59,19 @@ qvkey = f'{spc}_COLUMN'
 # Collect Daily Datasets
 # ----------------------
 #
-api = pyrsig.RsigApi(bbox=bbox, grid_kw=gdnam, workdir='temp')
+api = pyrsig.RsigApi(bbox=bbox, grid_kw=gdnam, workdir=gdnam)
 dates = pd.date_range(start, end)
 dt = pd.to_timedelta('1d') - pd.to_timedelta('1s')
 todss = []
 cqdss = []
 for bdate in dates:
   edate = bdate + dt
-  cqdss.append(pyrsig.open_ioapi(bdate.strftime(qtmpl))[[qvkey]])
+  # cqdss.append(pyrsig.open_ioapi(bdate.strftime(qtmpl))[[qvkey]])
+  cqdss.append(
+      api.to_ioapi(cdkey, bdate=bdate, edate=edate).where(
+          lambda x: x > -9.999e+36
+      )
+  )
   todss.append(
       api.to_ioapi(tdkey, bdate=bdate, edate=edate).where(
           lambda x: x > -9.999e+36
@@ -76,10 +87,10 @@ tods = xr.concat(todss, dim='TSTEP')
 cqds = xr.concat(cqdss, dim='TSTEP')
 # sample CMAQ when TropOMI is valid
 tcqds = cqds.where(~tods[tvkey].isnull().isel(LAY=0))
-mcqds = tcqds.mean('TSTEP')
-mtods = tods.mean('TSTEP')
+mcqds = tcqds.mean('TSTEP', keepdims=True)
+mtods = tods.mean('TSTEP', keepdims=True)
 # CMAQ stores NO2_COLUMN and HCHO column as petamolecules/cm2
-mtods[f'{tvkey}_CMAQ'] = mcqds[qvkey] * 1e15 
+mtods[f'{tvkey}_CMAQ'] = mcqds[qvkey].dims, mcqds[qvkey].data * 1e15 
 mtods[f'{tvkey}_CMAQ'].attrs.update(
     units='molecules/cm**2',
     long_name=qvkey.ljust(16),
@@ -89,4 +100,9 @@ mtods[f'{tvkey}_CMAQ'].attrs.update(
 # Output TropOMI with CMAQ
 # ------------------------
 #
-mtods.to_netcdf(f'TropOMI_and_CMAQ_AVG_{tvkey}_{start}_{end}.nc')
+mtods.to_netcdf(f'TropOMI_and_CMAQ_AVG_{tvkey}_{start}_{end}_{gdnam}.nc')
+fig, axx = plt.subplots(1, 2, figsize=(12, 4))
+qm = mtods[tvkey].plot(ax=axx[0], vmin=0, vmax=3e15, extend='max')
+mtods[f'{tvkey}_CMAQ'].plot(ax=axx[1], norm=qm.norm, extend='max')
+pycno.cno(mtods.crs_proj4).drawstates(ax=axx)
+fig.savefig(f'TropOMI_and_CMAQ_AVG_{tvkey}_{start}_{end}_{gdnam}.png')
